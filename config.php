136714389f6a90f6aa54d10b9e56baa41bc78686
Maxim Khokhlov <xmv54080@gmail.com>
2017-06-17 12:06:54 +0300

    start

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://buy-by-me.localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://buy-by-me.localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/WEB/www/buy-by-me.localhost/catalog/');
+define('DIR_SYSTEM', 'D:/WEB/www/buy-by-me.localhost/system/');
+define('DIR_LANGUAGE', 'D:/WEB/www/buy-by-me.localhost/catalog/language/');
+define('DIR_TEMPLATE', 'D:/WEB/www/buy-by-me.localhost/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/WEB/www/buy-by-me.localhost/system/config/');
+define('DIR_IMAGE', 'D:/WEB/www/buy-by-me.localhost/image/');
+define('DIR_CACHE', 'D:/WEB/www/buy-by-me.localhost/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/WEB/www/buy-by-me.localhost/system/storage/download/');
+define('DIR_LOGS', 'D:/WEB/www/buy-by-me.localhost/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/WEB/www/buy-by-me.localhost/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/WEB/www/buy-by-me.localhost/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'qazqaz');
+define('DB_DATABASE', 'buy-by-me');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
